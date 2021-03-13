56d414faa967f6de1e4bb6a634f3c0e61709850c
PaolaGarci <gnaomi394@gmail.com>
2021-03-13 09:55:33 -0600

    Create test.cpp
    Descripción de test, esta vacio.

---
+++ test.cpp
@@ -0,0 +1,21 @@
+#include "Inventorio.h"
+using namespace std;
+int main(){
+    //agregar 8 productos
+    prod1
+    //Modificar 1 producto
+
+    //quitar 1 producto
+
+    //agregar 3 clientes
+
+    //modificar 1 cliente
+
+    //quitar 1 cliente
+
+    //generar una orden 
+
+    // mostrar los datos de la orden generada
+
+    return 0;
+}
